eb18529057a30b262fd178aa5009673b70ea143b
Ashok <ashokcse505@gmail.com>
2023-12-13 12:58:57 +0530

    react-app

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Firebase Hosting!');
-});
-
+app.use(express.static("public"));
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
