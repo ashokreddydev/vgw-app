2cb47fe3ce383b2a2eb0d1b0be6116122f975752
Ashok <ashokcse505@gmail.com>
2023-12-15 13:58:49 +0530

    added

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 4000;
-const { Web3 } = require('web3');
 const bodyParser = require('body-parser');
 var asyncLoop = require('node-async-loop');
 const axios = require('axios');
